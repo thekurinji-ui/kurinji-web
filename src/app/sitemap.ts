68b7274cce1f5247a2bca967dea49d6eadbf2ff2
ALNO <thekurinji@gmail.com>
2026-07-15 01:49:30 +0700

    Update sitemap.ts

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,19 +3,47 @@ import { siteConfig } from "@/config/site";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const routes = [
-    "",
-    "/about",
-    "/ecosystem",
-    "/blog",
-    "/contact",
-    "/privacy-policy",
-    "/terms-and-conditions",
+    {
+      path: "",
+      priority: 1,
+      changeFrequency: "weekly" as const,
+    },
+    {
+      path: "/ecosystem",
+      priority: 0.9,
+      changeFrequency: "weekly" as const,
+    },
+    {
+      path: "/about",
+      priority: 0.8,
+      changeFrequency: "monthly" as const,
+    },
+    {
+      path: "/blog",
+      priority: 0.8,
+      changeFrequency: "weekly" as const,
+    },
+    {
+      path: "/contact",
+      priority: 0.6,
+      changeFrequency: "yearly" as const,
+    },
+    {
+      path: "/privacy-policy",
+      priority: 0.3,
+      changeFrequency: "yearly" as const,
+    },
+    {
+      path: "/terms-and-conditions",
+      priority: 0.3,
+      changeFrequency: "yearly" as const,
+    },
   ];
 
   return routes.map((route) => ({
-    url: `${siteConfig.url}${route}`,
+    url: `${siteConfig.url}${route.path}`,
     lastModified: new Date(),
-    changeFrequency: route === "" ? "weekly" : "monthly",
-    priority: route === "" ? 1 : 0.6,
+    changeFrequency: route.changeFrequency,
+    priority: route.priority,
   }));
 }
