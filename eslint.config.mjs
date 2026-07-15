97a46052be2666993b8cbed86a4c7c17b21df005
ALNO <thekurinji@gmail.com>
2026-07-15 09:41:04 +0700

    Create eslint.config.mjs

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat({ baseDirectory: import.meta.dirname });
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "no-duplicate-imports": "error",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+  },
+];
+
+export default eslintConfig;
